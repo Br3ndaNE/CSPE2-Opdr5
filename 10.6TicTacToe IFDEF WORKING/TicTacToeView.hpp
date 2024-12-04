6969bbfa94ceb796cc525eefe46c99f8b243a5f9
Br3ndaNE <77840520+Br3ndaNE@users.noreply.github.com>
2024-12-04 15:40:42 +0100

    10.6 Ifdef working

---
+++ 10.6TicTacToe IFDEF WORKING/TicTacToeView.hpp
@@ -0,0 +1,16 @@
+#ifndef TICTACTOEVIEW_HPP
+#define TICTACTOEVIEW_HPP
+
+#include <iostream>
+#include <utility>
+
+class TicTacToeView {
+public:
+    virtual ~TicTacToeView() = default;
+
+    virtual void UpdateBoard(char board[3][3]) = 0;
+    virtual std::pair<int,int> ClickEvent() = 0;
+    virtual bool isOpen() = 0;
+};
+
+#endif // TICTACTOEVIEW_HPP
